5ce23b59428b735da0958641fe667975eac1da01
douglasmatosdev <douglasmatosdev@gmail.com>
2021-01-23 13:30:09 -0300

    add comments

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 
+// Create server
 const app = express()
 
+// Run server
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+
+// Route Home
 app.get('/', (req, res) => res.send('Você está na HOME, tente em /atendimentos agora'))
+
+// Route atendimentos
 app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos'))
