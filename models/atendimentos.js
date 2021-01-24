7742441192d270b68522c4a24791b1d241e5432d
douglasmatosdev <douglasmatosdev@gmail.com>
2021-01-24 14:34:11 -0300

    Create method DELETE

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -82,6 +82,18 @@ class Atendimento {
             }
         })
     }
+
+    delete(id, res) {
+        const sql = 'DELETE FROM Atendimentos WHERE id=?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
 }
 
 module.exports = new Atendimento
